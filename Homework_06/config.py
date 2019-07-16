e783b2cdcbca4828fc74e5f5fa5f89c54505bf8a
zarawang85 <50597267+zarawang85@users.noreply.github.com>
2019-07-15 23:55:59 -0400

    Add files via upload

---
+++ Homework_06/config.py
@@ -0,0 +1,4 @@
+api_key = "Ceoe8oNFdFXVttD8YzNRMqp71FosqjBz"
+api_key_weather = "55ef2f571a72890aed28dcf59e6ec29a"
+gkey = "AIzaSyDlbwx5uu4RuUoBoc4YK0HAGHFY4GIFR8w"
+api_key_census = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
